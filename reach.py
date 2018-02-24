f124e6fd4942273d39cf2b07691a3e835387976c
D Sohn <jungbaesohn@gmail.com>
2018-02-24 13:22:05 -0800

    completed basic ET implementation, now regenerating tool API

--- reach.py
+++ reach.py
@@ -4,6 +4,13 @@ from scipy.integrate import ode
 from scipy import linalg
 from scipy import special, optimize
 import matplotlib.pyplot as plt
+import pdb
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import matlab.engine
+
+import sys
+
 
 def ell_valign(v,x):
 
@@ -81,28 +88,36 @@ def deriv_ea_nodist(t,y,n,sys):
     # "received" + str(y)
     X = np.reshape(y, (n, n)) #back to matrix
     A = sys.A
-    F = sys.F
+    #F = sys.F #gives complete bogus
+    F = np.linalg.inv(sys.F) #verify THIS PROBABLY SINCE ADJOINT EQUATION IS -A
     BPB = sys.BPB
 
     L0 = sys.L0
 
     p1 = np.sqrt(np.linalg.multi_dot([L0.T, F, BPB, F.T, L0]))
-    #print "p1" + str(p1)
+    #print "TIME: " + str(t)
+    #print "p1 " + str(p1)
 
-    #print "F"+ str(F)
-    #print "L0" + str(L0)
-    #print "X" + str(X)
-    #print "y" + str(y)
+
+    #print "F "+ str(F)
+    #print "L0 " + str(L0)
+    #print "X " + str(X)
+    #print "y " + str(y)
     p2 = np.sqrt(np.linalg.multi_dot([L0.T, F, X, F.T, L0]))
-    #print "p2" + str(p2)
+    #print "p2 " + str(p2)
+
+    if abs(p1) < sys.abs_tol:
+        p1 = sys.abs_tol
+    if abs(p2) < sys.abs_tol:
+        p2 = sys.abs_tol
 
 
     pp1 = p1/p2
 
-    #print "pp1" + str(pp1)
+    #print "pp1 " + str(pp1)
 
     pp2 = p2/p1
-    #print "pp2" + str(pp2)
+    #print "pp2 " + str(pp2)
     dxdt = np.dot(A,X) + np.dot(X,A.T) + pp1*X + pp2*BPB
 
     #print "dxdt" + str(dxdt)
@@ -116,7 +131,15 @@ def deriv_ea_nodist(t,y,n,sys):
 
 def update_sys(sys,t_now,t0):
 
+    #print "t_now: " + str(t_now)
+    #print "t0: " + str(t0)
+    #print "sys.A"
+
     sys.F = linalg.expm((sys.A)*(t_now-t0))
+
+    #print sys.A
+    #print "sys.F"
+    #print sys.F
 
     return sys
 
@@ -166,13 +189,38 @@ def system_init():
 
     sys.num_search = 3
     sys.n = 3
-
     sys.t0 = 0
-    sys.t1 = 3
+    sys.t1 = 10
     sys.dt=0.01
 
     sys.len_prop = int(np.ceil(float((sys.t1 - sys.t0) / sys.dt))) + 2
     return sys
+
+def switch_system(sys):
+    #L2 = 1
+    #R2 = 2
+    #A2 = [0 -1/C; 1/L2 -R2/L2]
+    #B2 = [1/C 0; 0 1/L2]
+    A = np.array([[0, -10],
+                  [1, -2],
+                  ])
+
+    sys.A = np.reshape(A, (2, 2))  # packed in matrix form
+
+    B = np.array([[10, 0],
+                  [0, 1],
+                  ])
+
+    sys.B = np.reshape(B, (2, 2))  # packed in matrix form
+
+    P = np.array([[1, 0],
+                  [0, 1],
+                  ])
+
+    sys.P = np.reshape(P, (2, 2))  # packed in matrix form
+
+    sys.BPB = np.linalg.multi_dot([sys.B, sys.P, sys.B.T])
+
 
 def system_init_LC():
 
@@ -208,7 +256,11 @@ def system_init_LC():
     sys.P = np.reshape(P, (2, 2))  # packed in matrix form
 
 
-    sys.BPB = np.linalg.multi_dot([sys.B, sys.P, sys.B ])
+    sys.BPB = np.linalg.multi_dot([sys.B, sys.P, sys.B.T ])
+
+    #print "sys.BPB"
+
+    #print sys.BPB
 
     sys.L = np.array([[1, 0],
                       [0, 1]
@@ -237,22 +289,29 @@ def system_init_LC():
 
 def reach_vtx(sys,num_spokes_per_slice,unit_spokes,center_trajectory,reach_set):
 
-    for t in range(sys.len_prop): #for each time prop
-        print t
+    V = np.empty([3, 1])
+    for t in range(sys.len_prop-1): #for each time prop
+        #print t
 
         wheel_slice = np.zeros((2,num_spokes_per_slice), dtype=np.float)
         for spoke_index in range(num_spokes_per_slice):
 
-            print spoke_index
+            #print spoke_index
             spoke = unit_spokes[:,spoke_index]
             mval = sys.abs_tol #initializing spoke length max value search
 
             for ellipsoid_index in range(sys.num_search):
-                print ellipsoid_index
+                #print ellipsoid_index
 
                 #max spoke radius search (EA)
                 #print np.reshape(reach_set[:,4, 1], (sys.n, sys.n))
-                Q = np.reshape(reach_set[:,t, ellipsoid_index], (sys.n, sys.n))
+
+                #INVERSION IS HAPPENING DUE TO DEFINITION OF ELLIPSOID USING SHAPE MATRIX
+                Q = np.reshape(reach_set[:, t, ellipsoid_index], (sys.n, sys.n))
+                Q = np.linalg.inv(Q)
+                Q = Q.T
+                #Q = np.reshape(reach_set[:,t, ellipsoid_index], (sys.n, sys.n))
+                #Q = Q.T #as matlab does it...
                 v = np.linalg.multi_dot([spoke.T, Q, spoke])
                 if v > mval:
                     mval = v
@@ -263,14 +322,33 @@ def reach_vtx(sys,num_spokes_per_slice,unit_spokes,center_trajectory,reach_set):
             wheel_slice[:,spoke_index] = adjusted_spoke
         #just finished wheel slice per time
 
-        #create time stamp copies for slice points here
+        #create time stamp copies for slice points here [tt;X]
+        current_time = t*sys.dt
+
+        time_stamp = current_time*(np.ones(num_spokes_per_slice))
+        #print "current time" + str(current_time)
+        #print "wheel_slice"
+        #print wheel_slice
 
 
-    #return V
+        X = np.vstack((time_stamp, wheel_slice))
+
+        #print X
+        V = np.hstack((V,X))
+        #print V
+
+        #pdb.set_trace()
+        #tt = rs.time_values(ii) * ones(1, num_spokes_per_slice);
+        #    X = [tt; X];
+        #    V = [V X];
+
+
+
+    return V
 def reach_gui(sys,center_trajectory, reach_set):
     print "generating vertices and facets"
 
-    num_spokes_per_slice = 8
+    num_spokes_per_slice = 200
     phi = np.linspace(0,2*np.pi,num_spokes_per_slice)
     unit_spokes = np.array([np.cos(phi),np.sin(phi)])
     #print unit_spokes[0,:]
@@ -279,10 +357,54 @@ def reach_gui(sys,center_trajectory, reach_set):
     #print unit_spokes
     #print phi*180/np.pi
 
-    reach_vtx(sys,num_spokes_per_slice,unit_spokes,center_trajectory,reach_set)
+    vertices = reach_vtx(sys,num_spokes_per_slice,unit_spokes,center_trajectory,reach_set)
     #tube slice rendering vector numbers
     #sys.len_prop #time stamps
     F = triag_facets(num_spokes_per_slice, sys.len_prop)
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    #x = [0, 1, 1, 0]
+    #y = [0, 0, 1, 1]
+    #z = [0, 1, 0, 1]
+    #verts = [zip(x, y, z)]
+    verts = [zip(vertices[0,:],vertices[1,:],vertices[2,:])]
+    #print vertices[0,:]
+
+    #print verts
+    #verts = [zip(x, y, z)]  # [(0,0,0), (1,0,1), (1,1,0), (0,1,1)]
+    tri = Poly3DCollection(verts)  # Create polygons by connecting all of the vertices you have specified
+    #tri.set_color(colors.rgb2hex(sp.rand(3)))  # Give the faces random colors
+    tri.set_edgecolor('k')  # Color the edges of every polygon black
+    ax.add_collection3d(tri)  # Connect polygon collection to the 3D axis
+    plt.show()
+
+    #ax.add_collection3d(Poly3DCollection(vertices))
+    #plt.show()
+
+    # eng = matlab.engine.start_matlab()
+
+    # eng.triarea(nargout=0)
+
+    #ret = eng.triarea(1.0, 5.0)
+
+    #print vertices.shape
+    #print F.shape
+
+    #v_list = vertices.tolist()
+    #print v_list
+    #f_list = F.tolist()
+    #print f_list
+    #data_list = data.tolist()
+    #a = [data_list{:}]; x = cell2mat(a);
+    #y = double(reshape(x, 32, 32));
+    #eng = matlab.engine.connect_matlab()
+    #eng = matlab.engine.start_matlab()
+    #ret = eng.plotforme(v_list,f_list)
+    #print(ret)
+
+    #render with vertices and facets.
+
     #print F
 
 def reach_per_search(sys):
@@ -304,6 +426,9 @@ def reach_per_search(sys):
     i = 1
     while r.successful() and r.t <= t1:
         update_sys(sys, r.t, t0)  # update transition mat phi
+        #print "sys.F should be changing"
+        #print sys.F
+
         # update sys
         r.set_f_params(sys.n, sys)
         r.integrate(r.t + dt)
@@ -369,9 +494,43 @@ def reach_center(sys):
         #print r.y
 
     return center_trajectory
+def inspect_slice(reach_set,sys):
+    #expand this later to properly time stamped version
+    print " AT TIME N=0"
+    for i in range(sys.num_search):
+        print "reach_set at " + str(i)+ "th direction"
+        print sys.L[i].T
+        print reach_set[:,0,i]
+
+    print " AT TIME N=170"
+    for i in range(sys.num_search):
+        print "reach_set at " + str(i)+ "th direction"
+        print sys.L[i].T
+        print reach_set[:,170,i]
+    print " AT TIME N=171"
+    for i in range(sys.num_search):
+        print "reach_set at " + str(i) + "th direction"
+        print sys.L[i].T
+        print reach_set[:, 171, i]
+
+def evolve_nodist(reach_set,sys, extra_time):
+    switch_system(sys)
+
+    #sys.num_search = 2
+    #sys.n = 2
+    #sys.abs_tol = 0.0001
+    #sys.t0 = 0.0
+    #sys.t1 = 10.0
+    #sys.dt = float(float(sys.t1 - sys.t0)) / 200
+    #sys.len_prop = int(np.ceil(float((sys.t1 - sys.t0) / sys.dt))) + 2
+
+    #update system variables here
+
+    #calculate extended portion of evolution per direction
+    #concatenate
+
 def reach():
     sys = system_init_LC()
-
 
     print "starting center traj calculation"
     center_trajectory = reach_center(sys)
@@ -381,8 +540,6 @@ def reach():
     print center_trajectory[:,1]
     print "done center traj calculation"
 
-
-
     reach_set =np.empty((sys.n*sys.n, sys.len_prop, sys.num_search))
     #initialize the shape matrix tube
 
@@ -391,19 +548,21 @@ def reach():
         tube = reach_per_search(sys)
         reach_set[:,:,i] = tube    #time_series x vectorized shape matrix x search direction
 
+    inspect_slice(reach_set,sys)
 
+    #print "sys.BPB"
+    #print sys.BPB
+    #sample values here for debugging
 
     #graphing here after applying projection in orthogonal vector -> simply BB.T * shape matrix, where BB is normalized orthogonal basis
 
     print reach_set.shape
-    print np.reshape(reach_set[:,4,0], (sys.n, sys.n))
+    #print np.reshape(reach_set[:,170,0], (sys.n, sys.n))
 
-    print np.reshape(reach_set[:,4, 1], (sys.n, sys.n))
+    #print np.reshape(reach_set[:,170, 1], (sys.n, sys.n))
 
 
     reach_gui(sys,center_trajectory,reach_set)
-
-
 def ode_integrate():
     #Ab = np.array([[-0.25, 0, 0],
     #               [0.25, -0.2, 0],
@@ -443,8 +602,6 @@ def ode_integrate():
     print r.t
     print "done"
     print r.y
-
-
 def main():
     #v = np.random.randn(5,1)
     #x = np.random.randn(5,1)
@@ -464,4 +621,9 @@ def main():
 
 
 if __name__ == "__main__":
+
+    try:
+        print sys.argv[1]
+    except:
+        print " no sys arg "
     main()
